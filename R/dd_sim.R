f67d29543adeb83a2bdbef734dab6cae271b182d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-01-14 18:03:38 +0000

    Fix #15 ... and indentation

--- R/dd_sim.R
+++ R/dd_sim.R
@@ -104,100 +104,104 @@ dd_lamuN = function(ddmodel,pars,N)
 #' @export dd_sim
 dd_sim = function(pars,age,ddmodel = 1)
 {
-# Simulation of diversity-dependent process
-#  . start from crown age
-#  . no additional species at crown node
-#  . no missing species in present
-# pars = [la mu K]
-#  . la = speciation rate per species
-#  . mu = extinction rate per species
-#  . K = diversification carrying capacity
-#  . r = ratio of diversity-dependence in extinction rate over speciation rate
-# age = crown age
-# ddmodel = mode of diversity-dependence
-#  . ddmodel == 1 : linear dependence in speciation rate with parameter K
-#  . ddmodel == 1.3: linear dependence in speciation rate with parameter K'
-#  . ddmodel == 2 : exponential dependence in speciation rate
-#  . ddmodel == 2.1: variant with offset at infinity
-#  . ddmodel == 2.2: 1/n dependence in speciation rate
-#  . ddmodel == 2.3: exponential dependence in speciation rate with parameter x
-#  . ddmodel == 3 : linear dependence in extinction rate
-#  . ddmodel == 4 : exponential dependence in extinction rate
-#  . ddmodel == 4.1: variant with offset at infinity
-#  . ddmodel == 4.2: 1/n dependence in speciation rate
-#  . ddmodel == 5 : linear dependence in speciation rate and in extinction rate
-
-done = 0
-while(done == 0)
-{
-    # number of species N at time t
-    # i = index running through t and N
-    t = rep(0,1)
-    L = matrix(0,2,4)
-    i = 1
-    t[1] = 0
-    N = 2
-    # L = data structure for lineages,
-    # . L[,1] = branching times
-    # . L[,2] = index of parent species
-    # . L[,3] = index of daughter species
-    # . L[,4] = time of extinction
-    # j = index running through L
-    L[1,1:4] = c(0,0,-1,-1)
-    L[2,1:4] = c(0,-1,2,-1)
-    linlist = c(-1,2)
-    newL = 2
-    ff = dd_lamuN(ddmodel,pars,N[i])
-    laN = ff[1]
-    muN = ff[2]
-    denom = (laN + muN) * N[i]
-    t[i + 1] = t[i] + stats::rexp(1,denom)
-    while(t[i + 1] <= age)
+    # Simulation of diversity-dependent process
+    #  . start from crown age
+    #  . no additional species at crown node
+    #  . no missing species in present
+    # pars = [la mu K]
+    #  . la = speciation rate per species
+    #  . mu = extinction rate per species
+    #  . K = diversification carrying capacity
+    #  . r = ratio of diversity-dependence in extinction rate over speciation rate
+    # age = crown age
+    # ddmodel = mode of diversity-dependence
+    #  . ddmodel == 1 : linear dependence in speciation rate with parameter K
+    #  . ddmodel == 1.3: linear dependence in speciation rate with parameter K'
+    #  . ddmodel == 2 : exponential dependence in speciation rate
+    #  . ddmodel == 2.1: variant with offset at infinity
+    #  . ddmodel == 2.2: 1/n dependence in speciation rate
+    #  . ddmodel == 2.3: exponential dependence in speciation rate with parameter x
+    #  . ddmodel == 3 : linear dependence in extinction rate
+    #  . ddmodel == 4 : exponential dependence in extinction rate
+    #  . ddmodel == 4.1: variant with offset at infinity
+    #  . ddmodel == 4.2: 1/n dependence in speciation rate
+    #  . ddmodel == 5 : linear dependence in speciation rate and in extinction rate
+    
+    done = 0
+    while(done == 0)
     {
-        i = i + 1
-        ranL = sample2(linlist,1)
-        if((laN * N[i - 1] / denom) >= stats::runif(1))
+        # number of species N at time t
+        # i = index running through t and N
+        t = rep(0,1)
+        L = matrix(0,2,4)
+        i = 1
+        t[1] = 0
+        N = 2
+        # L = data structure for lineages,
+        # . L[,1] = branching times
+        # . L[,2] = index of parent species
+        # . L[,3] = index of daughter species
+        # . L[,4] = time of extinction
+        # j = index running through L
+        L[1,1:4] = c(0,0,-1,-1)
+        L[2,1:4] = c(0,-1,2,-1)
+        linlist = c(-1,2)
+        newL = 2
+        ff = dd_lamuN(ddmodel,pars,N[i])
+        laN = ff[1]
+        muN = ff[2]
+        denom = (laN + muN) * N[i]
+        t[i + 1] = t[i] + stats::rexp(1,denom)
+        while(t[i + 1] <= age)
         {
-            # speciation event
-            N[i] = N[i - 1] + 1
-            newL = newL + 1
-            L = rbind(L,c(t[i],ranL,sign(ranL) * newL,-1))
-            linlist = c(linlist,sign(ranL) * newL)
-        } else {
-            # extinction event
-            N[i] = N[i - 1] - 1
-            L[abs(ranL),4] = t[i]
-            w = which(linlist == ranL)
-            linlist = linlist[-w]
-            linlist = sort(linlist)
+            i = i + 1
+            ranL = sample2(linlist,1)
+            if((laN * N[i - 1] / denom) >= stats::runif(1))
+            {
+                # speciation event
+                N[i] = N[i - 1] + 1
+                newL = newL + 1
+                L = rbind(L,c(t[i],ranL,sign(ranL) * newL,-1))
+                linlist = c(linlist,sign(ranL) * newL)
+            } else {
+                # extinction event
+                N[i] = N[i - 1] - 1
+                L[abs(ranL),4] = t[i]
+                w = which(linlist == ranL)
+                linlist = linlist[-w]
+                linlist = sort(linlist)
+            }
+            if(sum(linlist < 0) == 0 | sum(linlist > 0) == 0)
+            {
+                t[i + 1] = Inf
+            } else {
+                ff = dd_lamuN(ddmodel,pars,N[i])
+                laN = ff[1]
+                muN = ff[2]
+                denom = (laN + muN) * N[i]
+                if (denom == 0) {
+                    t[i + 1] = Inf # no more event will happen -> conclude sim
+                } else {
+                    t[i + 1] = t[i] + stats::rexp(1,denom)
+                }
+            } 
         }
         if(sum(linlist < 0) == 0 | sum(linlist > 0) == 0)
         {
-            t[i + 1] = Inf
+            done = 0
         } else {
-            ff = dd_lamuN(ddmodel,pars,N[i])
-            laN = ff[1]
-            muN = ff[2]
-            denom = (laN + muN) * N[i]
-            t[i + 1] = t[i] + stats::rexp(1,denom)
-        } 
+            done = 1
+        }
     }
-    if(sum(linlist < 0) == 0 | sum(linlist > 0) == 0)
-    {
-       done = 0
-    } else {
-       done = 1
-    }
-}
-
-L[,1] = age - c(L[,1])
-notmin1 = which(L[,4] != -1)
-L[notmin1,4] = age - c(L[notmin1,4])
-L[which(L[,4] == age + 1),4] = -1
-tes = L2phylo(L,dropextinct = T)
-tas = L2phylo(L,dropextinct = F)
-brts = L2brts(L,dropextinct = T)
-out = list(tes = tes,tas = tas,L = L,brts = brts)
-return(out)
-
+    
+    L[,1] = age - c(L[,1])
+    notmin1 = which(L[,4] != -1)
+    L[notmin1,4] = age - c(L[notmin1,4])
+    L[which(L[,4] == age + 1),4] = -1
+    tes = L2phylo(L,dropextinct = T)
+    tas = L2phylo(L,dropextinct = F)
+    brts = L2brts(L,dropextinct = T)
+    out = list(tes = tes,tas = tas,L = L,brts = brts)
+    return(out)
+    
 }
