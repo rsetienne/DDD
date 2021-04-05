39d4e9bb81d4ff3b87547a3076c4da09f6a1ad76
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-04-05 18:59:07 +0200

    test new ddmodels behave as expected

---
+++ tests/testthat/test_ddmodels.R
@@ -0,0 +1,209 @@
+context("test_ddmodels")
+
+# I do not test models 1 through 4; these have been implemented for a long time
+# and I assume they have been thoroughly tested
+
+# Case 1.: 0 < r < Inf (or 0 < alpha < 1)
+pars_set1 <- c(
+  "lambda_0" = 0.8,
+  "mu_0" = 0.2,
+  "K" = 20,
+  "r" = 1/3 # corresponds to alpha = 1/4
+)
+# cat(paste("Testing ddmodels with lambda_0 =", pars_set1[1], "mu_0 =", pars_set1[2], "K =", pars_set1[3],"alpha =", round(pars_set1[4], 3), "\n"))
+# Rates obtained on paper
+exptd_rates_set1 <- list(
+  #"lambda_cst"    = function(N) 0.8, # not with alpha != 1
+  #"mu_cst"        = function(N) 0.2, # not with alpha != 0
+  "lambda_lin"     = function(N) pmax(0.8 - 0.0225 * N, 0),
+  "mu_lin"         = function(N) 0.2 + 0.0075 * N,
+  "lambda_exp"     = function(N) pmax(0.8 * N ^ (-log(0.8 / 0.35) / log(20)), 0),
+  "mu_exp"         = function(N) 0.2 * N ^ (log(1.75) / log(20)),
+  "lambda_exp_alt" = function(N) pmax(0.8 * (7 / 16) ^ (N / 20), 0),
+  'mu_exp_alt'     = function(N) 0.2 * (7 / 4) ^ (N / 20)
+)
+
+# Case 2.: r = 0 (alpha = 0)
+pars_set2 <- c(
+  "lambda_0" = 0.8,
+  "mu_0" = 0.2,
+  "K" = 20,
+  "r" = 0
+)
+exptd_rates_set2 <- list(
+  #"lambda_cst"    = function(N) 0.8, # not with alpha != 1
+  #"mu_cst"        = function(N) 0.2, # not with alpha != 0
+  "lambda_lin"     = function(N) pmax(0.8 - 0.03 * N, 0),
+  "mu_lin"         = function(N) rep(0.2, length(N)),
+  "lambda_exp"     = function(N) pmax(0.8 * N ^ (-log(4) / log(20)), 0),
+  "mu_exp"         = function(N) rep(0.2, length(N)),
+  "lambda_exp_alt" = function(N) pmax(0.8 * (1/4) ^ (N / 20), 0),
+  'mu_exp_alt'     = function(N) rep(0.2, length(N))
+)
+# Case 3.: r = Inf (alpha = 1)
+pars_set3 <- c(
+  "lambda_0" = 0.8,
+  "mu_0" = 0.2,
+  "K" = 20,
+  "r" = Inf
+)
+exptd_rates_set3 <- list(
+  #"lambda_cst"    = function(N) 0.8, # not with alpha != 1
+  #"mu_cst"        = function(N) 0.2, # not with alpha != 0
+  "lambda_lin"     = function(N) rep(0.8, length(N)),
+  "mu_lin"         = function(N) 0.2 + 0.03 * N,
+  "lambda_exp"     = function(N) rep(0.8, length(N)),
+  "mu_exp"         = function(N) 0.2 * N ^ (log(4) / log(20)),
+  "lambda_exp_alt" = function(N) rep(0.8, length(N)),
+  'mu_exp_alt'     = function(N) 0.2 * 4 ^ (N / 20)
+)
+
+# Declare test functions
+## Match a ddmodel digit with a set of speciation and extinction function
+## See example below in exptd_rates_set1
+match_exptd_rates <- function(ddmodel, exptd_rates_set, n_seq) {
+  rates_ls <- switch(
+    as.character(ddmodel),
+    "5" = list("la_N" = exptd_rates_set$lambda_lin(n_seq), "mu_N" = exptd_rates_set$mu_lin(n_seq)),
+    "6" = list("la_N" = exptd_rates_set$lambda_lin(n_seq), "mu_N" = exptd_rates_set$mu_exp(n_seq)),
+    "7" = list("la_N" = exptd_rates_set$lambda_exp(n_seq), "mu_N" = exptd_rates_set$mu_exp(n_seq)),
+    "8" = list("la_N" = exptd_rates_set$lambda_exp(n_seq), "mu_N" = exptd_rates_set$mu_lin(n_seq)),
+    #"9" = list("la_N" = exptd_rates_set$lambda_exp_alt(n_seq), "mu_N" = exptd_rates_set$mu_cst(n_seq)),
+    #"10" = list("la_N" = exptd_rates_set$lambda_cst(n_seq), "mu_N" = exptd_rates_set$mu_exp_alt(n_seq)),
+    "11" = list("la_N" = exptd_rates_set$lambda_lin(n_seq), "mu_N" = exptd_rates_set$mu_exp_alt(n_seq)),
+    "12" = list("la_N" = exptd_rates_set$lambda_exp_alt(n_seq), "mu_N" = exptd_rates_set$mu_exp_alt(n_seq)),
+    "13" = list("la_N" = exptd_rates_set$lambda_exp_alt(n_seq), "mu_N" = exptd_rates_set$mu_lin(n_seq)),
+  )
+  return(rates_ls)
+}
+## Test function; compare DDD output with rates obtained on paper
+test_dd_loglik_rhs_precomp <- function(ddmodel, pars_set, exptd_rates_set) {
+  # global variables
+  N <- pars_set["K"]
+  x <- rep(NA, 10) # length of the Q_n vector
+  n_seq <- c(0, 0:(length(x) + 2 * N)) # based on internal code, not sure why
+  lnn <- length(n_seq)
+  
+  exptd_rates <- match_exptd_rates(
+    ddmodel = ddmodel,
+    exptd_rates_set = exptd_rates_set,
+    n_seq = n_seq
+  )
+  ddd_output <- dd_loglik_rhs_precomp(
+    pars = c("pars" = pars_set, "k" = N, "ddmodel" = ddmodel), x = x
+  )
+  ddd_rates <- list("la_N" = ddd_output[1:lnn], "mu_N" = ddd_output[(lnn + 1):(2 * lnn)])
+  # Test
+  cat(paste("Testing ddmodel =", ddmodel, "\n"))
+  expect_equal(ddd_rates, exptd_rates)
+}
+
+test_lambdamu <- function(ddmodel, pars_set, exptd_rates_set) {
+  # global variables
+  N <- pars_set["K"]
+  x <- rep(NA, 10) # length of the Q_n vector
+  n_seq <- c(0, 0:(length(x) + 2 * N)) # based on internal code, not sure why
+  lnn <- length(n_seq)
+  
+  exptd_rates <- match_exptd_rates(
+    ddmodel = ddmodel,
+    exptd_rates_set = exptd_rates_set,
+    n_seq = n_seq
+  )
+  ddd_rates <- lambdamu(n = n_seq, pars = pars_set, ddep = ddmodel)
+  names(ddd_rates) <- c("la_N", "mu_N")
+  # Test
+  cat(paste("Testing ddmodel =", ddmodel, "\n"))
+  expect_equal(ddd_rates, exptd_rates)
+}
+
+test_dd_lamuN <- function(ddmodel, pars_set, exptd_rates_set) {
+  # global variables
+  N <- pars_set["K"]
+  x <- rep(NA, 10) # length of the Q_n vector
+  n_seq <- c(0, 0:(length(x) + 2 * N)) # based on internal code, not sure why
+  
+  exptd_rates <- match_exptd_rates(
+    ddmodel = ddmodel,
+    exptd_rates_set = exptd_rates_set,
+    n_seq = n_seq
+  )
+  ddd_rates <- list(
+    "la_N" = purrr::map_dbl(n_seq, function(n) {
+      dd_lamuN(ddmodel = ddmodel, pars = pars_set, N = n)[1]
+    }),
+    "mu_N" = purrr::map_dbl(n_seq, function(n) {
+      dd_lamuN(ddmodel = ddmodel, pars = pars_set, N = n)[2]
+    })
+  )
+  # Test
+  cat(paste("Testing ddmodel =", ddmodel, "\n"))
+  expect_equal(ddd_rates, exptd_rates)
+}
+
+test_that("set1", {
+  ddmodels <- c(5:8, 11:13)
+  purrr::walk(
+    ddmodels, 
+    test_dd_loglik_rhs_precomp, 
+    pars_set = pars_set1, 
+    exptd_rates_set = exptd_rates_set1
+  )
+  purrr::walk(
+    ddmodels, 
+    test_lambdamu, 
+    pars_set = pars_set1, 
+    exptd_rates_set = exptd_rates_set1
+  )
+  purrr::walk(
+    ddmodels, 
+    test_dd_lamuN, 
+    pars_set = pars_set1, 
+    exptd_rates_set = exptd_rates_set1
+  )
+})
+
+test_that("set2", {
+  ddmodels <- c(5:8, 11:13)
+  purrr::walk(
+    ddmodels, 
+    test_dd_loglik_rhs_precomp, 
+    pars_set = pars_set2, 
+    exptd_rates_set = exptd_rates_set2
+  )
+  purrr::walk(
+    ddmodels, 
+    test_lambdamu, 
+    pars_set = pars_set2, 
+    exptd_rates_set = exptd_rates_set2
+  )
+  purrr::walk(
+    ddmodels, 
+    test_dd_lamuN, 
+    pars_set = pars_set2, 
+    exptd_rates_set = exptd_rates_set2
+  )
+})
+
+test_that("set3", {
+  ddmodels <- c(5:8, 11:13)
+  purrr::walk(
+    ddmodels, 
+    test_dd_loglik_rhs_precomp, 
+    pars_set = pars_set3, 
+    exptd_rates_set = exptd_rates_set3
+  )
+  purrr::walk(
+    ddmodels, 
+    test_lambdamu, 
+    pars_set = pars_set3, 
+    exptd_rates_set = exptd_rates_set3
+  )
+  purrr::walk(
+    ddmodels, 
+    test_dd_lamuN, 
+    pars_set = pars_set3, 
+    exptd_rates_set = exptd_rates_set3
+  )
+})
+
