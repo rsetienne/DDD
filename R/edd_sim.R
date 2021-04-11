73f915451e15f2161881cc37a96e6794eaeef87e
rsetienne <rampaletienne@gmail.com>
2021-04-11 16:41:25 +0200

    New version of ED

--- R/edd_sim.R
+++ R/edd_sim.R
@@ -1,77 +1,70 @@
-edd_update_lamu <- function(ED, params, model, i) {
+edd_update_lamu <- function(ED, ED_max, params, model) {
+  N <- params[1]
+  la0 <- params[2]
+  mu0 <- params[3]
+  beta_N <- params[4]
+  beta_phi <- params[5]
   if (model == "dsce2") {
     if (length(params) != 5) {
       stop("incorrect parameter(s)")
     }
     #dependent speciation, constant extinction
-    N <- params[1]
-    la0 <- params[2]
-    mu0 <- params[3]
-    beta_N <- params[4]
-    beta_phi <- params[5]
-    newlas <- pmax(0,la0 + beta_N * N + beta_phi * dplyr::select_if(ED[i, ], !is.na(ED[i, ])))
-    newmus <- NA
+    if (beta_phi < 0) {
+      newlas <- pmax(0,la0 + beta_N * N + beta_phi * ED)
+    } else {
+      newlas <- pmax(0,la0 + beta_N * N + beta_phi * ED_max)
+    }
+    newmus <- rep(mu0,length(newlas))
   } else if (model == "dsde2") {
     if (length(params) != 7) {
       stop("incorrect parameter(s)")
     }
-    #dependent speciation, constant extinction
-    N <- params[1]
-    la0 <- params[2]
-    mu0 <- params[3]
-    beta_N <- params[4]
-    beta_phi <- params[5]
+    #dependent speciation, dependent extinction
     gamma_N <- params[6]
     gamma_phi <- params[7]
-    newlas <- pmax(0, la0 + beta_N * N + beta_phi * dplyr::select_if(ED[i, ], !is.na(ED[i, ])))
-    newmus <- pmax(0, mu0 + gamma_N * N + gamma_phi * dplyr::select_if(ED[i, ], !is.na(ED[i, ])))
+    if (beta_phi < 0) {
+      newlas <- pmax(0,la0 + beta_N * N + beta_phi * ED)
+    } else {
+      newlas <- pmax(0,la0 + beta_N * N + beta_phi * ED_max)
+    }
+    if (gamma_phi < 0) {
+      newmus <- pmax(0,mu0 + gamma_N * N + gamma_phi * ED)
+    } else {
+      newmus <- pmax(0,mu0 + gamma_N * N + gamma_phi * ED_max)
+    }
   }
-  
   return(list(newlas = newlas, newmus = newmus))
 }
 
-edd_sum_rates <- function(las, mus, i) {
-  return(sum(dplyr::select_if(las[i, ], !is.na(las[i, ]))) + sum(dplyr::select_if(mus[i, ], !is.na(mus[i, ]))))
+edd_sum_rates <- function(las, mus) {
+  return(sum(las) + sum(mus))
 }
 
-edd_sample_event <- function(las, mus, i) {
-  # add prefix to names of the rates to distinguish between different event types
-  rspec <- dplyr::select_if(las[i, ], !is.na(las[i, ]))
-  rext <- dplyr::select_if(mus[i, ], !is.na(mus[i, ]))
-  names(rspec) <- paste("rs", names(rspec), sep = "_")
-  names(rext) <- paste("re", names(rext), sep = "_")
-  
-  rates <- c(rspec, rext)
-  events <- names(rates)
-  
-  return(DDD::sample2(events, 1, prob = rates))
+edd_sample_event <- function(las, mus, linlist) {
+  events <- 1:(2 * length(linlist))
+  return(DDD::sample2(events, 1, prob = c(las,mus)))
 }
 
 edd_sim <- function (pars,
                      age,
-                     model = "dsce1",
+                     model = "dsce2",
                      metric = "pd",
                      offset = "none") {
   if (pars[1] <= 0 | pars[2] <= 0) {
     stop('per species rates should be positive')
   }
-  
   if (model == "dsce2" && length(pars) != 4) {
     stop('incorrect parameters')
   }
-  
   if (model == "dsde2" && length(pars) != 6) {
     stop('incorrect parameters')
   }
-  
   if (pars[2] <= 0) {
     stop('coefficient for extinction should be positive')
   }
-  
   if (metric != "pd" && offset != "none"){
     stop('only pd metric has offset methods')
   }
-  
 
     done <- 0
     while (done == 0) {
@@ -97,23 +90,19 @@ edd_sim <- function (pars,
       # controlling significant digits in tibble objects
       options(pillar.sigfig = 10)
       
-      if (metric == "ed"){
-        ED <- c(1, 1)
-        names(ED) <- c("t1", "t2")
-        las <- ED * pars[1]
-        las <- tibble::as_tibble_row(las)
-        mus <- ED * pars[2]
-        mus <- tibble::as_tibble_row(mus)
-      }else{
+      if (metric == "ed") {
+        ED <- c(0, 0)
+        ED_max <- as.vector(L2ED(L,age))
+        lamu <- edd_update_lamu(ED, ED_max, params, model)
+      } else {
         lamu <- matrix(c(pars[1], pars[2]), ncol = 2)
         Phi[i] <- 0
       }
-
       # ED values determine the total rates
       if (metric == "ed") {
         t[i + 1] <-
-          t[i] + stats::rexp(1, edd_sum_rates(las, mus, i))
-      } else{
+          t[i] + stats::rexp(1, edd_sum_rates(lamu$newlas, lamu$newmus))
+      } else {
         t[i + 1] <-
           t[i] + stats::rexp(1, pdd_sum_rates(lamu, N, i))
       }
@@ -121,18 +110,8 @@ edd_sim <- function (pars,
       # main simulation circle
       while (t[i + 1] <= age) {
         i <- i + 1
-        
-        # new algorithm to deal with non-constant rates
-        if (metric == "ed") {
-          t_new <-
-            t[i - 1] + stats::rexp(1, edd_sum_rates(las, mus, i - 1))
-        } else{
-          t_new <- t[i - 1] + stats::rexp(1, pdd_sum_rates(lamu, N, i - 1))
-        }
-        
-        if (t_new <= t[i])
-          t[i] <- t_new
-        
+        #print(i)
+        #print(t[i])
         if (metric == "pd"){
           if (offset == "none") {
             Phi[i] <- L2Phi(L, t[i], metric)
@@ -146,45 +125,46 @@ edd_sim <- function (pars,
         }
         
         if (metric == "ed") {
-          ED <- dplyr::bind_rows(ED, L2ED(L, t[i]))
-          newlamus <- edd_update_lamu(ED, params, model, i)
-          las <- dplyr::bind_rows(las, newlamus$newlas)
-          
-          if(model == "dsce2"){
-            mus <- dplyr::bind_rows(mus, mus)
-          }else{
-            mus <- dplyr::bind_rows(mus, newlamus$newmus)
-          }
-          
-          # sample an event containing info of focal lineage and event type
-          event <-
-            edd_sample_event(las, mus, i)
-          # extract lineage number from event string
-          numL <- sub(".*_t", "", event)
-          # find out the lineage to match the sign in linlist
-          ranL <- linlist[match(numL, abs(linlist))]
-          
-          if (grepl("rs", event, fixed = TRUE)) {
-            N[i] <- N[i - 1] + 1
-            newL <- newL + 1
-            L <- rbind(L, c(t[i], ranL, sign(ranL) * newL,
-                            -1))
-            linlist <- c(linlist, sign(ranL) * newL)
-          } else if (grepl("re", event, fixed = TRUE)) {
-            N[i] <- N[i - 1] - 1
-            L[abs(ranL), 4] <- t[i]
-            w <- which(linlist == ranL)
-            linlist <- linlist[-w]
-            linlist <- sort(linlist)
-          } else if (grepl("rfs", event, fixed = TRUE) |
-                     grepl("rfe", event, fixed = TRUE)) {
+          ED <- as.vector(L2ED(L, t[i]))
+          lamu_real <- edd_update_lamu(ED, ED, params, model)
+          # sample whether event is fake or real
+          #print(lamu$newlas)
+          event <- sample(c('real','fake'),
+                          1,
+                          prob = c(sum(lamu_real$newlas + lamu_real$newmus),sum(lamu$newlas - lamu_real$newlas + lamu$newmus - lamu_real$newmus))
+                          )
+          #print(event)
+          if (event == 'real') {
+            # sample an event containing info of focal lineage and event type
+            event <- edd_sample_event(lamu_real$newlas, lamu_real$newmus, linlist)
+            #print(event)
+            ranL <- c(linlist,linlist)[event]
+            
+            if (event <= length(linlist)) {
+              N[i] <- N[i - 1] + 1
+              newL <- newL + 1
+              L <- rbind(L, c(t[i], ranL, sign(ranL) * newL, -1))
+              linlist <- c(linlist, sign(ranL) * newL)
+            } else {
+              N[i] <- N[i - 1] - 1
+              L[abs(ranL), 4] <- t[i]
+              w <- which(linlist == ranL)
+              linlist <- linlist[-w]
+              linlist <- sort(linlist)
+            }
+          } else {
             N[i] <- N[i - 1]
-          }
-          
+          }  
+          #print(linlist)
           if (sum(linlist < 0) == 0 | sum(linlist > 0) == 0) {
             t[i + 1] <- Inf
           } else {
-            t[i + 1] <- t[i] + stats::rexp(1, edd_sum_rates(las, mus, i))
+            ED <- as.vector(L2ED(L, t[i]))
+            ED_max <- as.vector(L2ED(L, age))
+            params[1] <- N[i]
+            lamu <- edd_update_lamu(ED, ED_max, params, model)
+            #print(lamu)
+            t[i + 1] <- t[i] + stats::rexp(1, edd_sum_rates(lamu$newlas, lamu$newmus))
           }
         } else {
           lamu <-
@@ -222,27 +202,12 @@ edd_sim <- function (pars,
             t[i + 1] <- t[i] + stats::rexp(1, pdd_sum_rates(lamu, N, i))
           }
         }
-
       }
       
       if (sum(linlist < 0) == 0 | sum(linlist > 0) == 0) {
         done <- 0
       } else {
         done <- 1
-      }
-    }
-    
-    if (metric == "ed") {
-      ED <- dplyr::bind_rows(ED, L2ED(L, t[i + 1]))
-      newlamus <- edd_update_lamu(ED, params, model, i + 1)
-      las <- dplyr::bind_rows(las, newlamus$newlas)
-      las <- las[-1, ]
-      if(model == "dsce2"){
-        mus <- dplyr::bind_rows(mus, mus)
-        mus <- mus[-1, ]
-      }else{
-        mus <- dplyr::bind_rows(mus, newlamus$newmus)
-        mus <- mus[-1, ]
       }
     }
     
@@ -264,12 +229,9 @@ edd_sim <- function (pars,
           tas = tas,
           L = L,
           brts = brts,
-          ED = ED,
-          las = las,
-          mus = mus,
           LTT = LTT
         )
-    }else{
+    } else {
       lamuphis <-
         data.frame(
           "time" = t[-i],
