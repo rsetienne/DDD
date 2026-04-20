f7a8c2818a03d1da3433b21e8c4bf8943a075ecc
rsetienne <rampaletienne@gmail.com>
2026-04-20 12:34:02 +0200

    Increase readability again

--- R/dd_utils.R
+++ R/dd_utils.R
@@ -841,8 +841,9 @@ optimizer <- function(
       fvalue[cy] <- out$fvalues
     }  
     cat(paste0('The maximum likelihood is: ', fvalue[cy], '.\n'))
-    cat(paste0('The corresponding parameters are: \n'))
+    cat(paste0('The corresponding parameters are: '))
     cat(untransform_pars(trparsopt))
+    cat('\n')
     if(cy > 1)
     {
       if(abs(fvalue[cy] - fvalue[cy - 1]) < optimpars[3])
